b41ff4c70d7f4175d2542d30267f9d655c45a100
Gonçalo Pinto <blanc@Goncalos-MacBook-Pro.local>
2023-04-17 18:08:59 +0100

    onNext message reordering

--- skel/src/main/java/causalop/CausalOperator.java
+++ skel/src/main/java/causalop/CausalOperator.java
@@ -5,11 +5,37 @@ import io.reactivex.rxjava3.core.ObservableOperator;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.observers.DisposableObserver;
 
+import javax.swing.*;
+import java.util.*;
+
 public class CausalOperator<T> implements ObservableOperator<T, CausalMessage<T>> {
     private final int n;
 
+    private List<CausalMessage<T>> queue;
+    private int[] seqNums;
+
     public CausalOperator(int n) {
         this.n = n;
+        this.seqNums = new int[2];
+        Arrays.fill(this.seqNums, 0);
+        this.queue = new ArrayList<>();
+    }
+
+    private Boolean canDeliver(int j, int[] v) {
+        if (v[j] != seqNums[j] + 1)
+            return false;
+        for (int i = 0; i < seqNums.length; i++)
+            if (i != j && v[i] > seqNums[i])
+                return false;
+        return true;
+    }
+
+    private CausalMessage<T> checkQueue() throws Exception{
+        for (CausalMessage<T> entry : queue) {
+            if (canDeliver(entry.j, entry.v))
+                return entry;
+        }
+        throw new Exception("Can't deliver");
     }
 
     @Override
@@ -17,7 +43,25 @@ public class CausalOperator<T> implements ObservableOperator<T, CausalMessage<T>
         return new DisposableObserver<CausalMessage<T>>() {
             @Override
             public void onNext(@NonNull CausalMessage<T> m) {
-                down.onNext(m.payload); // FIXME
+                if (canDeliver(m.j, m.v)) {
+                    // Deliver message
+                    down.onNext(m.payload);
+                    seqNums[m.j]++;
+
+                    // Retest queue
+                    while (true) {
+                        try {
+                            CausalMessage<T> msg = checkQueue();
+                            // Deliver message
+                            down.onNext(msg.payload); // Entrega
+                            seqNums[msg.j]++;         // Incrementa número de sequência
+                            queue.remove(msg);        // Remove da queue
+                        } catch (Exception e) {
+                            break;
+                        }
+                    }
+                }
+                else queue.add(m);
             }
 
             @Override
